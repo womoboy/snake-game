b013166b287a25fb1e0e871186a14753de63edcf
womoboy <reza2boyce@gmail.com>
2023-03-14 02:35:51 +0330

    crate snake,food with random position creator

--- javascript/script.js
+++ javascript/script.js
@@ -5,12 +5,43 @@ var blockSize = 25;
 var cols = 21;
 var rows = 21;
 
+// food
+var food = {};
+
+// snake
+var snake = {};
+
 window.onload = function() {
   frame = document.getElementById('frame');
   ctx = frame.getContext('2d');
   frame.width = cols * blockSize;
   frame.height = cols * blockSize;
 
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, frame.width, frame.height)
+  // spawn position snake
+  randomPlace(snake)
+
+  update();
+
 } 
+
+function update() {
+  // black screen
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, frame.width, frame.height);
+
+  // food 
+  randomPlace(food)
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x, food.y, blockSize, blockSize);
+
+  // snake
+  ctx.fillStyle = 'green';
+  ctx.fillRect(snake.x, snake.y, blockSize, blockSize);
+
+}
+
+// create random position
+function randomPlace(obj) {
+  obj.x = Math.floor(Math.random() * cols) * blockSize;
+  obj.y = Math.floor(Math.random() * rows) * blockSize;
+}
