5060aa3efdfb1e7babe90b525736d9a5ceb984d3
womoboy <reza2boyce@gmail.com>
2023-03-15 20:50:25 +0330

    add gameover, add snake body, eating food action

--- javascript/script.js
+++ javascript/script.js
@@ -4,12 +4,18 @@ var ctx;
 var blockSize = 25;
 var cols = 21;
 var rows = 21;
+var velocityX = 0;
+var velocityY = 0;
 
 // food
 var food = {};
 
 // snake
-var snake = {};
+var snake = {
+  x : 0,
+  y : 0,
+  body : [],
+};
 
 window.onload = function() {
   frame = document.getElementById('frame');
@@ -25,9 +31,9 @@ window.onload = function() {
   // listening for action like press keys
   document.addEventListener('keyup', moveHandler);
 
-  setInterval(update, 1000/10);
+  setInterval(update, 1000 / 10);
 
-} 
+}
 
 function update() {
   // black screen
@@ -40,7 +46,24 @@ function update() {
 
   // snake
   ctx.fillStyle = 'green';
+  snake.x += velocityX * blockSize;
+  snake.y += velocityY * blockSize;
   ctx.fillRect(snake.x, snake.y, blockSize, blockSize);
+  for(let i = 0; snake.body.length > i; i++) {
+    ctx.fillRect(snake.body[i][0], snake.body[i][1], blockSize, blockSize);
+  }
+  
+  // respown food when snake eate food
+  if(snake.x === food.x && snake.y === food.y) {
+    snake.body.push([food.x, food.y]);
+    console.log(snake.body);
+    randomPlace(food);
+  }
+
+  // if snake out of range screen should be head creash
+  if (snake.x < 0 || snake.y < 0 || snake.x >= frame.width || snake.y >= frame.height) {
+    gameOver('Snake head cresh');
+  }
 
 }
 
@@ -51,5 +74,22 @@ function randomPlace(obj) {
 }
 
 function moveHandler(e) {
-  console.log(e.code)
+  if(e.code == 'ArrowUp') {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.code == 'ArrowDown') {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.code == 'ArrowLeft') {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.code == 'ArrowRight') {
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
+
+function gameOver(str) {
+  alert(str);
+  return;
 }
